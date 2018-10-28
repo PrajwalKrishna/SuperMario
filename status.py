4f13f4e1954c4e75378e0dffe5b41a8b2e22d85e
Prajwal Krishna <prajwal.krishna@students.iiit.ac.in>
2018-10-28 13:10:57 +0530

    Pep8 on status.py also

--- status.py
+++ status.py
@@ -1,8 +1,11 @@
+'''Stores status of current game'''
 from time import time
 from level import Level
 
+
 class Status:
     '''Store game Status'''
+
     def __init__(self):
         self._life = 3
         self._level = 1
@@ -13,9 +16,11 @@ class Status:
         self._begin_time = time()
 
     def make_game(self):
-        level = Level(self._level * 100  ,self)
+        '''Starts a level'''
+        _ = Level(self._level * 100, self)
 
     def add_coin(self):
+        '''Increments coins'''
         self._coins += 1
         self._score += 250
         if self._coins == 50:
@@ -23,23 +28,30 @@ class Status:
             self._life += 1
 
     def update_time(self):
+        '''Maintains timer'''
         pre = self._time
         self._time = int(time() - self._begin_time)
         self._score += (self._time - pre)
 
-    def add_score(self,point):
+    def add_score(self, point):
+        '''Increments score by passed points'''
         self._score += point
 
     def add_kill(self):
+        '''Increments kills'''
         self._kills += 1
 
     def die(self):
-        #Game over
+        '''Restarts games on death of mario'''
+        # Game over
         self.make_game()
 
     def level_up(self):
+        '''Increments the level'''
         self._level += 1
         self.make_game()
 
     def print_status(self):
-        print('Level = ',self._level,"\tScore = ",self._score,'\t Coins = ',self._coins,'\t Lives = ',self._life,'\t Time = ',self._time,'\t Kills = ',self._kills)
+        '''Shows status on top of board'''
+        print('Level = ', self._level, "\tScore = ", self._score, '\t Coins = ', self._coins,
+              '\t Lives = ', self._life, '\t Time = ', self._time, '\t Kills = ', self._kills)
