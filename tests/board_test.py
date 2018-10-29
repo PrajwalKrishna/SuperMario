7ddff30f142a182f0c19d247010e8761dae70c93
Prajwal Krishna <prajwal.krishna@students.iiit.ac.in>
2018-10-29 19:08:59 +0530

    First set of test completed

--- tests/board_test.py
+++ tests/board_test.py
@@ -1,4 +1,5 @@
 '''Board class testing'''
+from random import randint
 from board import Board
 from status import Status
 from base import Point, Dimension
@@ -25,9 +26,24 @@ class TestBoard(Board):
             for j in range(point.y, dimensions.breadth + point.y):
                 assert shape[i - point.x][j - point.y] == self.matrix[i][j]
 
-    #def 
+
+    def test_update(self, point, dimensions):
+        '''This function tests update functions'''
+        shape = []
+        for _ in range(0, dimensions.length):
+            temp = []
+            for _ in range(0, dimensions.breadth):
+                temp.append(randint(0, 9))
+            shape.append(temp)
+
+        self.update(point, shape, dimensions)
+        for i in range(0, dimensions.length):
+            for j in range(0, dimensions.breadth):
+                assert shape[i][j] == self.matrix[point.x + i][point.y + j]
+
 
 STATUS = Status()
-BOARD = TestBoard(200, 100, STATUS)
-BOARD.test_init(200, 100, STATUS)
-BOARD.test_current(Point(2, 2), [], Dimension(5, 5) )
+BOARD = TestBoard(200 * randint(1, 4), 100 * randint(1 , 4), STATUS)
+BOARD.test_init(200 * randint(1, 4), 100 * randint(1, 4), STATUS)
+BOARD.test_current(Point(randint(0, 9), randint(0, 9)), [], Dimension(randint(2, 9), randint(2, 8)) )
+BOARD.test_update(Point(randint(0, 9), randint(0, 9)), Dimension( randint(3, 9), randint(2, 8)))
--- tests/status_test.py
+++ tests/status_test.py
@@ -1,68 +1,69 @@
-'''Test all functions related to status class'''
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+'''Test all functions related to self class'''
+from random import randint
 from status import Status
 
-#class Status(Status):
-#    '''Class for testing status functions'''
 
-# def test_init():
-#     '''Check constuction'''
-#     STATUS = Status()
-#     assert STATUS._life == 3
-#     assert STATUS._life == 3
-#     assert STATUS._level == 1
-#     assert STATUS._coins == 0
-#     assert STATUS._score == 0
-#     assert STATUS._time == 0
-#     assert STATUS._kills == 0
+class TestStatus(Status):
+
+    '''Class for testing self functions'''
+
+    def test_init(self):
+        '''Check constuction'''
+        assert self._life == 3
+        assert self._life == 3
+        assert self._level == 1
+        assert self._coins == 0
+        assert self._score == 0
+        assert self._time == 0
+        assert self._kills == 0
 
 
-# def test_add_coin():
-#     '''Function to test add_coin'''
-#     STATUS = Status()
-#     while True:
-#         coins = STATUS._coins
-#         score = STATUS._score
-#         life = STATUS._life
-#         STATUS.add_coin()
-#         if coins < 49:
-#             assert STATUS._coins == coins + 1 and STATUS._life == life
-#         else:
-#             assert STATUS._coins == 0 and STATUS._life == life + 1
-#             break
-        # assert STATUS._score == score + 250
+    def test_add_coin(self):
+        '''Function to test add_coin'''
+        while True:
+            coins = self._coins
+            score = self._score
+            life = self._life
+            self.add_coin()
+            if coins < 49:
+                assert self._coins == coins + 1 and self._life \
+                    == life
+            else:
+                assert self._coins == 0 and self._life == life + 1
+                break
+            assert self._score == score + 250
 
-def test_add_score():
-    '''Function to test add_score'''
-    point = 100
-    STATUS = Status()
-    score = STATUS._score
-    STATUS.add_score(point)
-    assert STATUS._score == score + point
-    point = 300
-    score = STATUS._score
-    STATUS.add_score(point)
-    assert STATUS._score == score + point
-#
-#
-# def test_add_kill():
-#     '''Function to test add_score'''
-#     STATUS = Status()
-#     kill = STATUS._kills
-#     STATUS.add_kill()
-#     assert STATUS._kills == kill + 1
-#
-# '''def test_level_up():
-#     Function to test add_score
-#     STATUS = Status()
-#     level = STATUS._level
-#     STATUS.level_up()
-#     assert STATUS._level == level + 1
-# '''
-#
-# #STATUS = Status()
-# #STATUS.test_init()
-# #STATUS.test_add_coin()
-# #STATUS.test_add_score(100)
-# #STATUS.test_add_score(200)
-# #STATUS.test_add_kill()
-# #STATUS.test_level_up()
+
+    def test_add_score(self, point):
+        '''Function to test add_score'''
+        score = self._score
+        self.add_score(point)
+        assert self._score == score + point
+        score = self._score
+        self.add_score(point)
+        assert self._score == score + point
+
+
+    def test_add_kill(self):
+        '''Function to test add_score'''
+        kill = self._kills
+        self.add_kill()
+        assert self._kills == kill + 1
+
+
+    def test_level_up(self):
+        '''Function to test level up'''
+        level = self._level
+        self.level_up()
+        assert self._level == level + 1
+
+
+STATUS = TestStatus()
+STATUS.test_init()
+STATUS.test_add_coin()
+STATUS.test_add_score(100 * randint(1, 9))
+STATUS.test_add_score(randint(50, 500))
+STATUS.test_add_kill()
+#STATUS.test_level_up()
